4fe99298b4def3fa7295d61fb2e2025692b1c1dc
Falaknaaz7944 <parmarfalak7@gmail.com>
2026-01-21 23:25:08 +0530

    daily progress

---
+++ list-prb.py
@@ -0,0 +1,17 @@
+#performing operations on list
+li = list(map(int, input().split()))
+
+li.insert(0, 5)
+li.insert(1, 10)
+li.insert(0, 6)
+print(li)
+
+li.remove(6)
+li.append(9)
+li.append(1)
+li.sort()
+print(li)
+
+li.pop()
+li.reverse()
+print(li)
