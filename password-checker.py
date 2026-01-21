20b32d5854277124374ccea2b358e77e3752dd78
Falaknaaz7944 <parmarfalak7@gmail.com>
2026-01-21 23:35:33 +0530

    daily progress

---
+++ password-checker.py
@@ -0,0 +1,10 @@
+#checks whether the input password is strong or week
+passwrd = input("enter your pass")
+if():
+    passwrd<=8
+    print("strong password")
+else:
+    print("weak password")
+
+
+    
